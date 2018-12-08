9ef0c1371540a57c8e693eea4126fb587282b087
pivaszbs <zhychek1@yandex.ru>
2018-12-08 17:02:38 +0300

    add CountDown and Carousel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './resources/styles.css'
+
 import Header from './components/header-footer/Header';
+import Featured from './components/featured/Featured';
+
 class App extends Component {
   render() {
     return (
       <div className="App" style={{ height:"1500px", background:'red'}}>
         <Header></Header>
+        <Featured/>
       </div>
     );
   }
