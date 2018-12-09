f15d84e6df58d55cedb6a52792408b4e79edd7dc
pivaszbs <zhychek1@yandex.ru>
2018-12-09 15:14:18 +0300

    higlight section

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './resources/styles.css'
 
 import Header from './components/header-footer/Header';
 import Featured from './components/featured/Featured';
+import VenueInfo from './components/venue-info/VenueInfo';
+import Higlights from './components/higlights/Higlights';
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{ height:"1500px", background:'red'}}>
+      <div className="App">
         <Header></Header>
         <Featured/>
+        <VenueInfo/>
+        <Higlights/>
       </div>
     );
   }
---
+++ src/components/higlights/Description.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+
+const Description = () => {
+    return (
+        <Fade>
+            <div className="center_wrapper">
+                <h2>Highlights</h2>
+                <div className="highlight_description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sagittis pulvinar leo, vel pulvinar tellus varius vel. Mauris tincidunt mattis varius. Praesent nec tellus hendrerit, semper mauris in, tempus lectus. Pellentesque ullamcorper sed neque nec hendrerit. Maecenas a leo porttitor, commodo lacus non, blandit mauris. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris mollis ligula eu lacus interdum imperdiet. Nulla facilisi. Mauris risus dolor, vehicula vitae leo eu, dignissim egestas magna. Nunc quis diam faucibus, interdum nisl quis, sagittis sapien. Donec sit amet dui lacinia, fringilla tellus quis, laoreet magna. Fusce egestas quam quis lectus elementum, eget.
+                </div> 
+            </div>
+        </Fade>
+    );
+};
+
+export default Description;
